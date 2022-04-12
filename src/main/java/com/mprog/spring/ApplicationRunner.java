df7dd6c3d80341251fae14a98fc11c15c91202a8
Munir Zikrullaev <munirprog@gmail.com>
2022-04-13 01:42:28 +0300

    xml based configuration

--- src/main/java/com/mprog/spring/ApplicationRunner.java
+++ src/main/java/com/mprog/spring/ApplicationRunner.java
@@ -5,22 +5,13 @@ import com.mprog.spring.database.repository.CompanyRepository;
 import com.mprog.spring.database.repository.UserRepository;
 import com.mprog.spring.ioc.Container;
 import com.mprog.spring.service.UserService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        Container container = new Container();
-
-        //        ConnectionPool connectionPool = new ConnectionPool();
-//        UserRepository userRepository = new UserRepository(connectionPool);
-//        CompanyRepository companyRepository = new CompanyRepository(connectionPool);
-//        UserService userService = new UserService(userRepository, companyRepository);
-
-//        ConnectionPool connectionPool = container.get(ConnectionPool.class);
-//        UserRepository userRepository = container.get(UserRepository.class);
-//        CompanyRepository companyRepository = container.get(CompanyRepository.class);
-        UserService userService = container.get(UserService.class);
-
-
+        var context = new ClassPathXmlApplicationContext("application.xml");
+        System.out.println(context.getBean("pool1", ConnectionPool.class));
     }
 }
+
