0e1244d01bebc4741d7348170ce6e812153dac82
Munir Zikrullaev <munirprog@gmail.com>
2022-04-14 00:59:05 +0300

    factory method injection

--- src/main/java/com/mprog/spring/ApplicationRunner.java
+++ src/main/java/com/mprog/spring/ApplicationRunner.java
@@ -12,6 +12,8 @@ public class ApplicationRunner {
     public static void main(String[] args) {
         var context = new ClassPathXmlApplicationContext("application.xml");
         System.out.println(context.getBean("pool1", ConnectionPool.class));
+        var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+        System.out.println(companyRepository);
     }
 }
 
