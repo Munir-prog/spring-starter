a6e4926e342d3e45e3f338c071d2b9988c3db3fe
Munir Zikrullaev <munirprog@gmail.com>
2022-04-15 01:29:24 +0300

    property injection

--- src/main/java/com/mprog/spring/ApplicationRunner.java
+++ src/main/java/com/mprog/spring/ApplicationRunner.java
@@ -2,9 +2,6 @@ package com.mprog.spring;
 
 import com.mprog.spring.database.pool.ConnectionPool;
 import com.mprog.spring.database.repository.CompanyRepository;
-import com.mprog.spring.database.repository.UserRepository;
-import com.mprog.spring.ioc.Container;
-import com.mprog.spring.service.UserService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
