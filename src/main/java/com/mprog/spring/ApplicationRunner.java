1fc0a5d4ecc42ffbb38e0308626a51d32188c1af
Munir Zikrullaev <munirprog@gmail.com>
2022-04-17 14:46:08 +0300

    Custom BeanPostProcessor second part.

--- src/main/java/com/mprog/spring/ApplicationRunner.java
+++ src/main/java/com/mprog/spring/ApplicationRunner.java
@@ -2,6 +2,7 @@ package com.mprog.spring;
 
 import com.mprog.spring.database.pool.ConnectionPool;
 import com.mprog.spring.database.repository.CompanyRepository;
+import com.mprog.spring.database.repository.CrudRepository;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
@@ -9,8 +10,8 @@ public class ApplicationRunner {
     public static void main(String[] args) {
         try (var context = new ClassPathXmlApplicationContext("application.xml")) {
             System.out.println(context.getBean("pool1", ConnectionPool.class));
-            var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
-            System.out.println(companyRepository);
+            var companyRepository = context.getBean("companyRepository", CrudRepository.class);
+            System.out.println(companyRepository.findById(1));
         }
     }
 }
--- src/main/java/com/mprog/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/mprog/spring/database/repository/CompanyRepository.java
@@ -1,16 +1,42 @@
 package com.mprog.spring.database.repository;
 
+import com.mprog.spring.bpp.Auditing;
+import com.mprog.spring.bpp.InjectBean;
+import com.mprog.spring.bpp.Transaction;
+import com.mprog.spring.database.entity.Company;
 import com.mprog.spring.database.pool.ConnectionPool;
 
-public class CompanyRepository {
+import javax.annotation.PostConstruct;
+import java.util.Optional;
 
-    private final ConnectionPool connectionPool;
+@Transaction
+@Auditing
+public class CompanyRepository implements CrudRepository<Integer, Company> {
 
-    public CompanyRepository(ConnectionPool connectionPool) {
-        this.connectionPool = connectionPool;
+    @InjectBean
+    private ConnectionPool connectionPool;
+
+    @PostConstruct
+    private void init() {
+        System.out.println("init company repository");
     }
 
-    public static CompanyRepository of(ConnectionPool connectionPool) {
-        return new CompanyRepository(connectionPool);
+    @Override
+    public Optional<Company> findById(Integer id) {
+        System.out.println("find by id method");
+        return Optional.of(new Company(id));
     }
+
+    @Override
+    public void delete(Company entity) {
+        System.out.println("delete entity");
+    }
+
+//    public CompanyRepository(ConnectionPool connectionPool) {
+//        this.connectionPool = connectionPool;
+//    }
+//
+//    public static CompanyRepository of(ConnectionPool connectionPool) {
+//        return new CompanyRepository(connectionPool);
+//    }
 }
