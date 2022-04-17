d0ee49982db9f2f5804d7a4c088c0938fabb51df
Munir Zikrullaev <munirprog@gmail.com>
2022-04-17 13:01:11 +0300

    Annotation-based configuration

--- src/main/java/com/mprog/spring/database/pool/ConnectionPool.java
+++ src/main/java/com/mprog/spring/database/pool/ConnectionPool.java
@@ -2,6 +2,8 @@ package com.mprog.spring.database.pool;
 
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.List;
 import java.util.Map;
 
@@ -27,6 +29,7 @@ public class ConnectionPool implements InitializingBean {
         this.properties = properties;
     }
 
+    @PostConstruct
     private void init() {
         System.out.println("Connection pool initialization");
     }
@@ -36,6 +39,7 @@ public class ConnectionPool implements InitializingBean {
         System.out.println("Connection pool initialization");
     }
 
+    @PreDestroy
     private void destroy() {
         System.out.println("Connection pool is cleaning");
     }
