0fbbb838ab7ff8e1e10a81b7af38557f1681b4a2
Munir Zikrullaev <munirprog@gmail.com>
2022-05-03 12:33:29 +0300

    Spring Boot. @Transactional

---
+++ src/test/java/com/mprog/spring/integration/database/repository/CompanyRepositoryTest.java
@@ -0,0 +1,51 @@
+package com.mprog.spring.integration.database.repository;
+
+import com.mprog.spring.database.entity.Company;
+import com.mprog.spring.integration.annotation.IT;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@IT
+@RequiredArgsConstructor
+@Transactional
+//@Rollback // by default
+@Commit
+class CompanyRepositoryTest {
+
+    private final EntityManager entityManager;
+
+    @Test
+    void findById() {
+        var company = entityManager.find(Company.class, 1);
+        assertNotNull(company);
+        assertThat(company.getLocales()).hasSize(2);
+    }
+
+
+    @Test
+    void save() {
+        var company = Company.builder()
+                .name("Apple")
+                .locales(Map.of(
+                        "ru", "Apple описание",
+                        "en", "Apple description"
+                ))
+                .build();
+        entityManager.persist(company);
+
+        assertNotNull(company.getId());
+    }
+
+}
