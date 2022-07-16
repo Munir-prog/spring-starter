577427530e39f16e929a393bc93ac1aa3bd6935f
Munir Zikrullaev <munirprog@gmail.com>
2022-07-16 09:31:18 +0300

    Spring Boot. Spring MVC testing

--- src/main/java/com/mprog/spring/dto/UserCreateEditDto.java
+++ src/main/java/com/mprog/spring/dto/UserCreateEditDto.java
@@ -2,10 +2,12 @@ package com.mprog.spring.dto;
 
 import com.mprog.spring.database.entity.Role;
 import lombok.Value;
+import lombok.experimental.FieldNameConstants;
 
 import java.time.LocalDate;
 
 @Value
+@FieldNameConstants
 public class UserCreateEditDto {
     String username;
     String firstname;
---
+++ src/test/java/com/mprog/spring/integration/http/controller/UserControllerTest.java
@@ -0,0 +1,49 @@
+package com.mprog.spring.integration.http.controller;
+
+import com.mprog.spring.dto.UserCreateEditDto;
+import com.mprog.spring.integration.IntegrationTestBase;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.shaded.org.hamcrest.collection.IsCollectionWithSize;
+
+import static com.mprog.spring.dto.UserCreateEditDto.Fields.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@AutoConfigureMockMvc
+@RequiredArgsConstructor
+class UserControllerTest extends IntegrationTestBase {
+
+    private final MockMvc mockMvc;
+
+    @Test
+    void findAll() throws Exception {
+        mockMvc.perform(get("/users"))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(view().name("user/users"))
+                .andExpect(model().attributeExists("users"));
+//                .andExpect(model().attribute("users", IsCollectionWithSize.hasSize(5)));
+    }
+
+    @Test
+    void create() throws Exception {
+        mockMvc.perform(post("/users")
+                .param(username, "test@gmail.com")
+                .param(firstname, "test")
+                .param(lastname, "gmail")
+                .param(role, "ADMIN")
+                .param(companyId, "1")
+        )
+                .andExpectAll(
+                    status().is3xxRedirection(),
+                        redirectedUrlPattern("/users/{\\d}")
+                );
+    }
+}
