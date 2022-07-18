bfa2d8b311c25ef626014fcbef4337578e4e3659
Munir Zikrullaev <munirprog@gmail.com>
2022-07-18 20:04:33 +0300

    Spring Boot. Thymeleaf

--- src/main/java/com/mprog/spring/http/controller/GreetingController.java
+++ src/main/java/com/mprog/spring/http/controller/GreetingController.java
@@ -30,6 +30,7 @@ public class GreetingController {
                          HttpServletRequest request,
                          @ModelAttribute("userReadDto") UserReadDto userReadDto) {
 //        model.addAttribute("user", new UserReadDto(1L, "Ivan"));
+        model.addAttribute("user", userReadDto);
         return "greeting/hello";
     }
 
