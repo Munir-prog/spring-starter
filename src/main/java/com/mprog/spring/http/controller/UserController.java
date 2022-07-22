b726d3db9747b61d36884d30c0888f38aac7c519
Munir Zikrullaev <munirprog@gmail.com>
2022-07-22 11:05:10 +0300

    Spring Boot. Thymeleaf. View layer P1

--- src/main/java/com/mprog/spring/http/controller/UserController.java
+++ src/main/java/com/mprog/spring/http/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.mprog.spring.http.controller;
 
+import com.mprog.spring.database.entity.Role;
 import com.mprog.spring.dto.UserCreateEditDto;
+import com.mprog.spring.service.CompanyService;
 import com.mprog.spring.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,6 +17,7 @@ import org.springframework.web.server.ResponseStatusException;
 public class UserController {
 
     private final UserService userService;
+    private final CompanyService companyService;
 
     @GetMapping
     public String findAll(Model model) {
@@ -29,6 +32,8 @@ public class UserController {
         return userService.findById(id)
                         .map(user -> {
                             model.addAttribute("user", user);
+                            model.addAttribute("roles", Role.values());
+                            model.addAttribute("companies", companyService.findAll());
                             return "user/user";
                         })
                                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
--- src/main/java/com/mprog/spring/service/CompanyService.java
+++ src/main/java/com/mprog/spring/service/CompanyService.java
@@ -5,28 +5,37 @@ import com.mprog.spring.database.repository.CompanyRepository;
 import com.mprog.spring.dto.CompanyReadDto;
 import com.mprog.spring.listener.entity.AccessType;
 import com.mprog.spring.listener.entity.EntityEvent;
+import com.mprog.spring.mapper.CompanyReadMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 public class CompanyService {
 
     private final CompanyRepository companyCrudRepository;
-    private final UserService userService;
     private final ApplicationEventPublisher eventPublisher;
+    private final CompanyReadMapper companyReadMapper;
 
     public Optional<CompanyReadDto> findById(Integer id) {
         return companyCrudRepository.findById(id)
                 .map(entity -> {
                     eventPublisher.publishEvent(new EntityEvent(entity, AccessType.READ));
-                    return new CompanyReadDto(entity.getId(), null);
+                    return companyReadMapper.map(entity);
                 });
+    }
+
+    public List<CompanyReadDto> findAll() {
+        return companyCrudRepository.findAll()
+                .stream()
+                .map(companyReadMapper::map)
+                .toList();
     }
 
 }
