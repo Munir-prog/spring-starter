80e25aa9c81546f63b19b1d450cbd1d472d4c2f4
Munir Zikrullaev <munirprog@gmail.com>
2022-07-27 11:41:14 +0300

    Password encoder

--- src/main/java/com/mprog/spring/config/SecurityConfiguration.java
+++ src/main/java/com/mprog/spring/config/SecurityConfiguration.java
@@ -1,9 +1,12 @@
 package com.mprog.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -11,11 +14,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
+//                .antMatcher("/registration").
                 .authorizeRequests().anyRequest().authenticated()
 //                .and().httpBasic(Customizer.withDefaults());
                 .and().formLogin(login -> login
                         .loginPage("/login")
                         .defaultSuccessUrl("/users")
                         .permitAll());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
--- src/main/java/com/mprog/spring/dto/UserCreateEditDto.java
+++ src/main/java/com/mprog/spring/dto/UserCreateEditDto.java
@@ -9,6 +9,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
@@ -19,6 +20,9 @@ public class UserCreateEditDto {
 
     @Email
     String username;
+
+    @NotBlank(groups = CreateAction.class)
+    String rawPassword;
 
     @Size(min = 3, max = 64)
     String firstname;
