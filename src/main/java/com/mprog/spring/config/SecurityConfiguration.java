12fd7caf4099ca8b361535421bb2c476fea4a4b9
Munir Zikrullaev <munirprog@gmail.com>
2022-07-27 15:27:34 +0300

    Authorization architecture

--- src/main/java/com/mprog/spring/config/SecurityConfiguration.java
+++ src/main/java/com/mprog/spring/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.mprog.spring.config;
 
+import com.mprog.spring.database.entity.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,6 +9,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import static com.mprog.spring.database.entity.Role.*;
+
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
@@ -15,17 +18,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
 //                .antMatcher("/registration").
-                .authorizeRequests().anyRequest().authenticated()
+                .authorizeHttpRequests(urlConfig -> urlConfig
+                        .antMatchers("/login", "/users/registration", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                        .antMatchers("/users/{\\d+}/delete").hasAuthority(ADMIN.getAuthority())
+                        .antMatchers("/admin/**").hasAuthority(ADMIN.getAuthority())
+                        .anyRequest().authenticated()
+
+                )
 //                .and().httpBasic(Customizer.withDefaults());
-                .and()
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/login")
                         .deleteCookies("JSESSIONID"))
                 .formLogin(login -> login
                         .loginPage("/login")
-                        .defaultSuccessUrl("/users")
-                        .permitAll());
+                        .defaultSuccessUrl("/users"));
     }
 
     @Bean
